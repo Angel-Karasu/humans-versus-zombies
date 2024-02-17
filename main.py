1e6162c878b5d291aaf4b7c9a32ab97d52ef129b
Angel-Karasu <angel.karasu@proton.me>
2024-02-17 14:07:19 +0100

    Rearrange names

--- main.py
+++ main.py
@@ -3,203 +3,203 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 nb = 175
-monde = 50
-son = 5
-vitZomb = 5
+world_area = 50
+song_area = 5
+zombie_speed = 5
 
-def barreProgression(progres, total):
-    pourcent = (progres/float(total))*100
-    barre = '█'*int(pourcent) + '-'*(100-int(pourcent))
-    return f'|{barre}| {pourcent:.2f}%'
+def progress_bar(progres, total):
+    percent = (progres/float(total))*100
+    bar = '█'*int(percent) + '-'*(100-int(percent))
+    return f'|{bar}| {percent:.2f}%'
 
-def moyenne(gen ,population):
+def average(gen ,population):
     return sum(stats[gen] for _, stats in population.items())/len(population)
 
-def creerPopulation(population = {}, nb = nb):
-    rd = [random.randint(0, monde), random.randint(0, monde)]
+def init_humans(population = {}, nb = nb):
+    rd = [random.randint(0, world_area), random.randint(0, world_area)]
     for i in range(len(population), nb+len(population)):
         population[f'humain {i+1}'] = {
-                'vitesse': random.randint(1, 10),
-                'vue': random.randint(1, 10),
+                'speed': random.randint(1, 10),
+                'vision': random.randint(1, 10),
                 'precision': random.randint(1, 10),
                 'position': rd,
-                'zoneBruit': [rd, rd],
-                'creer': time.time(),
+                'song_zone': [rd, rd],
+                'birth': time.time(),
             }
     return population
 
-def creerZombies():
+def init_zombies():
     zombies = []
     zombies.extend(
-        [random.randint(0, monde), random.randint(0, monde)]
+        [random.randint(0, world_area), random.randint(0, world_area)]
         for _ in range(nb)
     )
     return zombies
 
-def listeMortAppend(listeMort, stats):
-    listeMort.append({
-        'vitesse': stats['vitesse'],
-        'vue': stats['vue'],
+def add_death_list(death_liste, stats):
+    death_liste.append({
+        'speed': stats['speed'],
+        'vision': stats['vision'],
         'precision': stats['precision'],
-        'duree': (time.time() - stats['creer'])*1000
+        'time': (time.time() - stats['birth'])*1000
     })
-    return listeMort
+    return death_liste
 
-def coordMap(coord, vitesse):
-    if coord[0] > monde: coord[0] -= 2*vitesse
-    elif coord[0] < 0: coord[0] += 2*vitesse
+def coord_world(coord, speed):
+    if coord[0] > world_area: coord[0] -= 2*speed
+    elif coord[0] < 0: coord[0] += 2*speed
 
-    if coord[1] > monde: coord[1] -= 2*vitesse
-    elif coord[1] < 0: coord[1] += 2*vitesse
+    if coord[1] > world_area: coord[1] -= 2*speed
+    elif coord[1] < 0: coord[1] += 2*speed
     
     return [round(coord[0], 2), round(coord[1], 2)]
 
-def actionsHumain(population, zombies):
-    for humain, stats in population.items():
+def humans_actions(humans, zombies):
+    for human, stats in humans.items():
         pos = stats['position']
-        zB = [[pos[0], pos[1]], [pos[0], pos[1]]]
-        vit = stats['vitesse']
-        vue = stats['vue']
+        song_zone = [[pos[0], pos[1]], [pos[0], pos[1]]]
+        speed = stats['speed']
+        vision = stats['vision']
         for x, zombie in enumerate(zombies):
             coord = [pos[0] - zombie[0], pos[1] - zombie[1]]
-            if abs(coord[0]) <= vue and abs(coord[1]) <= vue:
+            if abs(coord[0]) <= vision and abs(coord[1]) <= vision:
                 if random.randint(1, 10)/2 <= stats['precision']/3: del zombies[x]
-                zB = [
-                    [zB[0][0] - son, zB[0][1] - son],
-                    [zB[1][0] + son, zB[1][1] + son],
+                song_zone = [
+                    [song_zone[0][0] - song_area, song_zone[0][1] - song_area],
+                    [song_zone[1][0] + song_area, song_zone[1][1] + song_area],
                 ]
 
-                population[humain]['position'] = [
-                    pos[0] + ((coord[0]/abs(coord[0] + 10**(-10)))*vit),
-                    pos[1] + ((coord[1]/abs(coord[1] + 10**(-10)))*vit),
+                humans[human]['position'] = [
+                    pos[0] + ((coord[0]/abs(coord[0] + 10**(-10)))*speed),
+                    pos[1] + ((coord[1]/abs(coord[1] + 10**(-10)))*speed),
                 ]
                 break
-        if population[humain]['position'] == pos:
-            population[humain]['position'] = [
-                pos[0] + random.choice((-1, 1))*vit,
-                pos[1] + random.choice((-1, 1))*vit,
+        if humans[human]['position'] == pos:
+            humans[human]['position'] = [
+                pos[0] + random.choice((-1, 1))*speed,
+                pos[1] + random.choice((-1, 1))*speed,
             ]
 
-        population[humain]['position'] = coordMap(population[humain]['position'], vit)
+        humans[human]['position'] = coord_world(humans[human]['position'], speed)
 
-        zB = [
-            [zB[0][0] - 1.5*vit, zB[0][1] - 1.5*vit],
-            [zB[1][0] + 1.5*vit, zB[1][1] + 1.5*vit],
+        song_zone = [
+            [song_zone[0][0] - 1.5*speed, song_zone[0][1] - 1.5*speed],
+            [song_zone[1][0] + 1.5*speed, song_zone[1][1] + 1.5*speed],
         ]
-        population[humain]['zoneBruit'] = zB
-    return [population, zombies]
+        humans[human]['song_zone'] = song_zone
+    return [humans, zombies]
 
-def actionsZombies(population, zombies, listeMort):
+def zombies_actions(humans, zombies, death_list):
     for x, zombie in enumerate(zombies):
-        pop = population.copy()
-        for humain, stats in pop.items():
+        hum = humans.copy()
+        for human, stats in hum.items():
             pos = stats['position']
-            zB = stats['zoneBruit']
+            song_zone = stats['song_zone']
             coord = [zombie[0] - pos[0], zombie[1] - pos[1]]
 
             if abs(coord[0]) <= 1 and abs(coord[1]) <= 1:
                 zombies.append([pos[0], pos[1]])
-                listeMort = listeMortAppend(listeMort, stats)
-                population.pop(humain)
-            elif zB[0][0] <= zombie[0] <= zB[1][0] and zB[0][1] <= zombie[1] <= zB[1][1]:
+                death_list = add_death_list(death_list, stats)
+                humans.pop(human)
+            elif song_zone[0][0] <= zombie[0] <= song_zone[1][0] and song_zone[0][1] <= zombie[1] <= song_zone[1][1]:
                 zombies[x] = [
-                    pos[0] + ((coord[0]/abs(coord[0] - 10**(-10)))*vitZomb),
-                    pos[1] + ((coord[1]/abs(coord[1] - 10**(-10)))*vitZomb),
+                    pos[0] + ((coord[0]/abs(coord[0] - 10**(-10)))*zombie_speed),
+                    pos[1] + ((coord[1]/abs(coord[1] - 10**(-10)))*zombie_speed),
                 ]
         
         if zombies[x] == zombie:
-            zombies[x] = [zombie[0] + random.choice((-1, 1))*vitZomb, zombie[1] + random.choice((-1, 1))*vitZomb]
+            zombies[x] = [zombie[0] + random.choice((-1, 1))*zombie_speed, zombie[1] + random.choice((-1, 1))*zombie_speed]
             
-        zombies[x] = coordMap(zombies[x], vitZomb)
+        zombies[x] = coord_world(zombies[x], zombie_speed)
 
-    return [population, zombies, listeMort]
+    return [humans, zombies, death_list]
 
-def actions(population, zombies, listeMort):
+def actions(humans, zombies, death_list):
     if random.random() >= 0.5:
-        population, zombies = actionsHumain(population, zombies)
-        population, zombies, listeMort = actionsZombies(population, zombies, listeMort)
+        humans, zombies = humans_actions(humans, zombies)
+        humans, zombies, death_list = zombies_actions(humans, zombies, death_list)
     else:
-        population, zombies, listeMort = actionsZombies(population, zombies, listeMort)
-        population, zombies = actionsHumain(population, zombies)
+        humans, zombies, death_list = zombies_actions(humans, zombies, death_list)
+        humans, zombies = humans_actions(humans, zombies)
     
-    return [population, zombies, listeMort]
+    return [humans, zombies, death_list]
 
-def tuerHumain(population, listeMort):
-    for _, stats in population.items():
-        listeMort = listeMortAppend(listeMort, stats)
-    return listeMort
+def kill_human(humans, death_list):
+    for _, stats in humans.items():
+        death_list = add_death_list(death_list, stats)
+    return death_list
 
-def creerNouvelleGen(listeMort):
-    population = {}
-    tempsTotal = sum(mort['duree'] for mort in listeMort)
-    listeMort = sorted(listeMort, key=lambda t: t['duree'], reverse=True)
+def create_new_gen(death_list):
+    humans = {}
+    total_time = sum(death['time'] for death in death_list)
+    death_list = sorted(death_list, key=lambda t: t['time'], reverse=True)
 
     for i in range((nb*3)//5):
         rand = random.random()
-        derniereProb = 0
+        last_prob = 0
 
-        for gen in listeMort:
-            prob = gen['duree']/tempsTotal
-            if rand >= derniereProb and rand < prob + derniereProb:
-                rd = [random.randint(0, monde), random.randint(0, monde)]
-                population[f'humain {i+1}'] = {
-                    'vitesse': gen['vitesse'],
-                    'vue': gen['vue'],
+        for gen in death_list:
+            prob = gen['time']/total_time
+            if rand >= last_prob and rand < prob + last_prob:
+                rd = [random.randint(0, world_area), random.randint(0, world_area)]
+                humans[f'humain {i+1}'] = {
+                    'speed': gen['speed'],
+                    'vision': gen['vision'],
                     'precision': gen['precision'],
                     'position': rd,
-                    'zoneBruit': [rd, rd],
-                    'creer': time.time(),
+                    'song_zone': [rd, rd],
+                    'birth': time.time(),
                 }
                 break
-            derniereProb += prob
-    population |= creerPopulation(population, (nb*2)//5)
-    return population
+            last_prob += prob
+    humans |= init_humans(humans, (nb*2)//5)
+    return humans
 
-nbGen = 1000
-print(barreProgression(0, nbGen), end='\r')
-population = creerPopulation()
+nb_gen = 100
+print(progress_bar(0, nb_gen), end='\r')
+humans = init_humans()
 l = []
-stats = ['vitesse', 'vue', 'precision']
-statsListe = [[], [] , []]
+stats = ['speed', 'vision', 'precision']
+stats_list = [[], [] , []]
 
-for i in range(nbGen):
+for i in range(nb_gen):
     l.append(i)
-    for x in range(len(stats)): statsListe[x].append(moyenne(stats[x], population))
+    for x in range(len(stats)): stats_list[x].append(average(stats[x], humans))
 
-    zombies = creerZombies()
-    listeMort = []
+    zombies = init_zombies()
+    death_list = []
     k = 0
-    while len(population) > 0 and len(zombies) > 0 and k < 2*(monde + nb):
-        population, zombies, listeMort = actions(population, zombies, listeMort)
+    while len(humans) > 0 and len(zombies) > 0 and k < 2*(world_area + nb):
+        humans, zombies, death_list = actions(humans, zombies, death_list)
         k += 1
 
-    if len(population) > 0:
-        vic = f'\rLes humains ont gagné, il en restait {len(population)}'
-        listeMort = tuerHumain(population, listeMort)
-    else: vic = f'\rLes zombies ont gagné, il en restait {len(zombies)}'
+    if len(humans) > 0:
+        winners = f'\rHumans won, {len(humans)} were still alive'
+        death_list = kill_human(humans, death_list)
+    else: winners = f'\rZombies won, {len(zombies)} were still alive'
 
-    population = creerNouvelleGen(listeMort)
-    print(vic, '  ',barreProgression(i+1, nbGen), end='\r')
+    humans = create_new_gen(death_list)
+    print(winners, ' :\t',progress_bar(i+1, nb_gen), end='\r')
 print('\n')
 
 
-plt.xlabel('Génération')
-plt.ylabel('Moyenne des gènes')
+plt.xlabel('Generation')
+plt.ylabel('Gene average')
 
-m, b = np.polyfit(l, statsListe[0], 1)
+m, b = np.polyfit(l, stats_list[0], 1)
 aj_lineaire = np.poly1d([m, b])
-plt.plot(l, aj_lineaire(l), label='Vitesse', color='#294DBA')
-plt.plot(l, statsListe[0], label='Vitesse Point', color='#1B327A', alpha=0.2)
+plt.plot(l, aj_lineaire(l), label='speed', color='#294DBA')
+plt.plot(l, stats_list[0], label='speed Point', color='#1B327A', alpha=0.2)
 
-m, b = np.polyfit(l, statsListe[1], 1)
+m, b = np.polyfit(l, stats_list[1], 1)
 aj_lineaire = np.poly1d([m, b])
-plt.plot(l, aj_lineaire(l), label='Vue', color='#9AFA1E')
-plt.plot(l, statsListe[1], label='Vue Point', color='#6FAD1D', alpha=0.2)
+plt.plot(l, aj_lineaire(l), label='vision', color='#9AFA1E')
+plt.plot(l, stats_list[1], label='vision Point', color='#6FAD1D', alpha=0.2)
 
-m, b = np.polyfit(l, statsListe[2], 1)
+m, b = np.polyfit(l, stats_list[2], 1)
 aj_lineaire = np.poly1d([m, b])
 plt.plot(l, aj_lineaire(l), label='Précision', color='#FA9D43')
-plt.plot(l, statsListe[2], label='Précision Point', color='#AD7237', alpha=0.2)
+plt.plot(l, stats_list[2], label='Précision Point', color='#AD7237', alpha=0.2)
 
 plt.legend(loc='upper left')
 plt.show()
